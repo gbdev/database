66b100b0200996f034bce3b9125775370211cee4
Antonio Vivace <puma95.95@gmail.com>
2018-01-16 23:20:41 +0100

    Building game.json, starting single game scraping

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,42 +1,87 @@
 from bs4 import BeautifulSoup
 import requests
+import json
+import re
 
 r = requests.get('http://privat.bahnhof.se/wb800787/gb/demos/by_id_asc.html')
+
+# Sluuurp
 soup = BeautifulSoup(r.text, 'html5lib')
-#print(soup.prettify())
+
+# print(soup.prettify())
 
 # Header of the table
-soup.body.center.tbody.contents[1].center.table.tbody.contents[0]
+#  soup.body.center.tbody.contents[1].center.table.tbody.contents[0]
 
 entries = 0
 baseURL = 'http://privat.bahnhof.se/wb800787/gb'
 
+intervalStart = 0
+intervalEnd = 2
+
 for r, row in enumerate(soup.body.center.tbody.contents[1].center.table.tbody.contents):
-    if (r != 0 and r < 400):
+    # TODO: use range
+    if (r != intervalStart and r < intervalEnd):
+        validEntry = 0
         print('\nRow', r)
+
         for c, column in enumerate(row.contents):
             if (c == 0):
                 print('ID:', row.contents[c].contents[0])
             elif (c == 1):
-                print('Title:', row.contents[c].a.contents[0])
-                print('URL:', row.contents[c].a.get('href'))
-                print('Complete URL:', baseURL + row.contents[c].a.get('href')[2::])
+                gameTitle = column.a.contents[0]
+                gameURL = baseURL + column.a.get('href')[2::]
+                print('Title:', gameTitle)
+                print('URL:', column.a.get('href'))
+                print('Complete URL:', gameURL)
+
             elif (c == 2):
-                print('Developer:', row.contents[c].a.contents[0])
+                developer = column.a.contents[0]
+                print('Developer:', developer)
             elif (c == 3):
-                platform = row.contents[c].contents[0]
+                platform = column.contents[0]
                 print('Platform:', platform)
                 if (platform == 'CGB' or platform == 'DMG' or platform == 'DMG,CGB'):
-                    entries+=1
+                    validEntry = 1
+                    # use dict and remove this inhumanity
+                    if (platform == 'DMG,CGB'):
+                        platform = 'GB'
+                    if (platform == 'CGB'):
+                        platform = 'GBC'
+                    if (platform == 'DMG'):
+                        platform = 'GB'
             elif (c == 4):
-                #print(row.contents[c])
-                if (row.contents[c].contents):
-                    print('Year:', row.contents[c].contents[0])
+                # print(row.contents[c])
+                if (column.contents):
+                    year = column.contents[0]
+                    print('Year:', year)
             elif (c == 5):
-                if (row.contents[c].a):
-                    print('File URL:', row.contents[c].a.get('href'))
-                    print('Complete File URL:', baseURL + row.contents[c].a.get('href')[2::])
+                if (column.a):
+                    relativeFileURL = column.a.get('href')[2::]
+                    # Harcoded for /files/ , use a regex
+                    fileName = relativeFileURL[7::]
+                    fileURL = baseURL + column.a.get('href')[2::]
+                    print('File URL:', column.a.get('href'))
+                    print('Complete File URL:', fileURL)
             else:
                 print(c, column)
+        
+        if (validEntry):
+            r2 = requests.get(gameURL)
+            gameSoup = BeautifulSoup(r2.text, 'html5lib')
+
+            game = dict( title = gameTitle,
+                         slug = gameTitle, # Slugify
+                         developer = developer,
+                         platform = platform,
+                         typetag = 'demo',
+                         screenshots = '', # TODO
+                         rom = '', # TODO
+                         date = year,
+                         files = [[fileName, 'release']]
+                         ) 
+            entries+=1
+            print(json.dumps(game))
+            
 
 print(entries, 'total DMG and GBC entries')
