5759564fe8bf0d19eb9507f26ede08435fb3f851
cwallace3421 <cwallace3421@gmail.com>
2018-12-03 20:08:55 +0000

    Add jsdoc for all functions

--- scrapers/nesworld/filesystem.js
+++ scrapers/nesworld/filesystem.js
@@ -7,6 +7,11 @@ const path = require('path');
 const url = 'http://www.nesworld.com/';
 const base = path.resolve(__dirname, 'entries');
 
+/**
+ * Push game meta and downloaded contents to the file system asynchronously (kind of).
+ * @param {Object[]} meta - An array of game meta objects.
+ * @returns {Promise} Promise that will resolve when all game meta is processed and complete.
+ */
 exports.push = (meta) => {
     if (fs.existsSync(base)) rimraf(base);
     fs.mkdirSync(base);
@@ -20,15 +25,24 @@ exports.push = (meta) => {
     return Promise.all(promises);
 }
 
+/**
+ * Given game meta, download screenshots, download rom zip, and build folder structure for game.
+ * @async
+ * @param {Object} game - The game meta.
+ */
 async function handleGameMeta(game) {
     const gamePath = path.resolve(base, game.slug);
     fs.mkdirSync(gamePath);
 
     // Download Screenshots
-    for (let i = 0; i < game.screenshots.length; i++) {
-        const screenshotFilename = cleanPath(game.screenshots[i]);
-        await download(url + game.screenshots[i], path.resolve(gamePath, screenshotFilename));
-        game.screenshots[i] = screenshotFilename;
+    if (game.screenshots.length === 1 && game.screenshots[0] === 'placeholder.png') {
+        fs.copyFileSync(path.resolve(__dirname, 'placeholder.png'), path.resolve(gamePath, 'placeholder.png'));
+    } else {
+        for (let i = 0; i < game.screenshots.length; i++) {
+            const screenshotFilename = cleanPath(game.screenshots[i]);
+            await download(url + game.screenshots[i], path.resolve(gamePath, screenshotFilename));
+            game.screenshots[i] = screenshotFilename;
+        }
     }
 
     // Download ROM zip and extract
@@ -41,26 +55,12 @@ async function handleGameMeta(game) {
     fs.writeFileSync(path.resolve(gamePath, 'game.json'), JSON.stringify(game, null, 4));
 }
 
-function download(url, dest) {
-    return new Promise((resolve, reject) => {
-        const file = fs.createWriteStream(dest);
-        http.get(url, function (response) {
-            response.pipe(file);
-            file.on('finish', function () {
-                file.close(() => {
-                    resolve();
-                });
-            });
-        }).on('error', function (err) { // Handle errors
-            fs.unlink(dest); // Delete the file async. (But we don't check the result)
-            if (err) {
-                log.error(url, err, dest);
-                reject(err.message);
-            }
-        });
-    });
-};
-
+/**
+ * Unzip a zip file to a directory with the slug name, keep only rom files, delete the rest of the contents.
+ * @param {string} slug - The slug of the game name.
+ * @param {string} zipFilePath - The file path for the zip file.
+ * @returns {string} The potential main rom file name.
+ */
 function unzip(slug, zipFilePath) {
     let stage = 'Loading ZIP: ' + zipFilePath;
     try {
@@ -108,14 +108,54 @@ function unzip(slug, zipFilePath) {
     }
 }
 
+/**
+ * Helper function to get the file name and extension from a file path.
+ * @param {string} p - The file path.
+ * @returns {string} The file name with the extension.
+ */
 function cleanPath(p) {
     return path.basename(p);
 }
 
+/**
+ * Helper function to lower case, remove all spaces, and special characters from a name.
+ * @param {string} n - The name to clean.
+ * @returns {string} The cleaned name.
+ */
 function cleanName(n) {
     return n.toLowerCase().replace(/[\s\/]/g, '-').replace(/[^\w\-\.]/g, '');
 }
 
+/**
+ * Helper function to asynchronously download a file from a url.
+ * @param {string} url - The url to download.
+ * @param {string} dest - The file path to put the downloaded file.
+ * @returns {Promise} Promise that will resolve when the download is complete.
+ */
+function download(url, dest) {
+    return new Promise((resolve, reject) => {
+        const file = fs.createWriteStream(dest);
+        http.get(url, function (response) {
+            response.pipe(file);
+            file.on('finish', function () {
+                file.close(() => {
+                    resolve();
+                });
+            });
+        }).on('error', function (err) { // Handle errors
+            fs.unlink(dest); // Delete the file async. (But we don't check the result)
+            if (err) {
+                log.error(url, err, dest);
+                reject(err.message);
+            }
+        });
+    });
+}
+
+/**
+ * Helper function to synchronously remove the contents of a directory.
+ * @param {string} dirPath - The directory path.
+ */
 function rimraf(dirPath) {
     if (fs.existsSync(dirPath)) {
         fs.readdirSync(dirPath).forEach(function(entry) {
--- scrapers/nesworld/log.js
+++ scrapers/nesworld/log.js
@@ -1,5 +1,9 @@
 const chalk = require('chalk');
 
+/**
+ * Log out a title.
+ * @param {string} title
+ */
 exports.title = (title) => {
     console.info();
     console.info(chalk.green('**********'));
@@ -7,22 +11,39 @@ exports.title = (title) => {
     console.info(chalk.green('**********'));
 }
 
+/**
+ * Log out a error.
+ * @param {string} name
+ * @param {Error} exception
+ * @param {string[]} messages
+ */
 exports.error = (name, exception, ...messages) => {
     console.error();
     console.error(chalk.red('Error: '));
     for (let i = 0; i < messages.length; i++) {
         console.error(chalk.red('> ' + messages[i]));
     }
+    if (name) {
+        console.error(chalk.red('> '), name);
+    }
     if (exception) {
-        console.error(chalk.red('> '), exception)
+        console.error(chalk.red('> '), exception.toString().trim());
     }
     console.error();
 }
 
+/**
+ * Log out a info message.
+ * @param {string} message
+ */
 exports.info = (message) => {
     console.info(chalk.green('Info: ') + message);
 }
 
+/**
+ * Log out a warning message.
+ * @param {string} message
+ */
 exports.warn = (message) => {
     console.warn(chalk.yellow('Warn: ') + message);
 }
