e5b6a5de040c16fad05565c4d763e8cd5a1f5e16
Dag7 <dag7@protonmail.com>
2021-08-20 10:40:33 +0200

    Add Pouet scraper (#80)
    * pouet-importer: added __pycache__ folder to .gitignore
    
    * pouet: single prod scraper, all prods scraper
    
    * pouet: build function, now it fetches file and proper clean everything
    
    * pouet: slug guidelines / build: manages ftp, zip and clean unneeded files / logger
    
    * pouet: fix not working warning suppression, added proper new line in log
    
    * pouet: managed the case of no multiple pages
    
    * pouet: fixed a typo
    
    * pouet: added JSON file foreach prod's folder
    
    * pouet: updated global json + now it should work for each specified platform
    
    * pouet: final revision, updated blacklist, refactored code (just a little)
    
    * update: gitignore, new files to exclude; gamesList is no longer formatted (because of Python)
    
    * add: readme and instructions + requirements
    
    * removed gamesList.json + refactoring
    - added DEBUG flag in py_common/utils.py -> now you just need to enable or disable a flag to enable or disable certain functions, like advanced logging or downloading in a special folder
    - while DEBUG flag is active, the script will do its things in a special folder (called beta)
    - added a function in utils.py -> if a folder doesnt exist, this fn will create it
    - files and folders hierarchy updated: since we can't import from a top-level package, I've decided to do a folder which will contains a shared common folder and several importers. Importers have a lot of things in common each other, that's why a shared folder is required
    
    * updated: gitignore + removed gamesList.json
    
    * update: missing capital L logger letter
    
    * upd: production no longer requires mandatory lic
    
    * hotfixes: utils and pouet
    utils.py: removed useless functions, updated flags
    pouet.py: check area not needed anymore
    
    * enhancement: added BETA_FOLDER check
    
    * fixed a typo
    
    * utils hotfix: path
    
    * utils: added TMP_FOLDER
    
    * Remove validate from requirements
    
    * Restore gamesList.json
    
    * enhancements: log where you want + unknown exts
    - you can choose to log on file or on console
    - manage unknown exts (before, pouet.py created an empty folder
    
    Co-authored-by: Antonio Vivace <avivace4@gmail.com>

---
+++ scrapers/py_importers/py_common/Production.py
@@ -0,0 +1,28 @@
+class Production:
+    def __init__(self, title, slug, developer, platform, typetag, screenshots, files,
+    lic="", assetLicense="", description="", video="", date="", tags=[], alias="", repository="", gameWebsite="", devWebsite="", onlineplay="",
+    wip="", url = ""):
+        # mandatory fields
+        self.title = title
+        self.slug = slug
+        self.developer = developer
+        self.platform = platform
+        self.typetag = typetag
+        self.screenshots = screenshots
+        self.files = files
+
+        # optional fields
+        self.lic = lic if lic else ""
+        self.assetLicense = assetLicense if assetLicense else ""
+        self.description = description if description else ""
+        self.video = video if video else ""
+        self.date = date if date else ""
+        self.tags = tags if tags is not None else []
+        self.alias = alias if alias is not None else []
+        self.repository = repository if repository else ""
+        self.gameWebsite = gameWebsite if gameWebsite else ""
+        self.devWebsite = devWebsite if devWebsite else ""
+        self.onlineplay = onlineplay if onlineplay else ""
+        self.wip = wip if wip else ""
+
+        self.url = url
---
+++ scrapers/py_importers/py_common/utils.py
@@ -0,0 +1,213 @@
+import sys
+import py_common.utils
+import re
+import json
+import shutil
+import zipfile
+import fnmatch
+import urllib3
+import requests
+import unicodedata
+import contextlib
+import urllib
+from urllib.request import urlopen
+
+import os
+from os import listdir
+from os.path import isfile, join
+
+from bs4 import BeautifulSoup
+from unidecode import unidecode
+
+from py_common.Logger import Logger
+from py_common.Production import Production
+
+###########################
+### GLOBAL VAR AND CONS ###
+###########################
+DEBUG = True                    # enable if you want a more detailed log, beta folder and other useful things 
+CLEANZIP = True                 # enable if you want to delete downloaded zip file 
+BETA_FOLDER = "beta"            # warning: this must not be blank. If you dont want to use this simply set DEBUG to False
+TMP_FOLDER = "tmp"              # warning: this must not be blank. It is used to store tmp files.
+PREFERRED_OUTPUT = "CONSOLE"    # change it to LOG if you need to output everything in log.txt file
+
+logger = Logger(PREFERRED_OUTPUT)
+
+# required: we need to check if BETA_FOLDER and TMP_FOLDER exist or not
+if not BETA_FOLDER or not TMP_FOLDER or BETA_FOLDER == "" or TMP_FOLDER == "":
+    print("BETA_FOLDER or TMP_FOLDER can't be empty!")
+    exit(1)
+if not os.path.isdir("py_common/" + BETA_FOLDER):
+    os.mkdir("py_common/" + BETA_FOLDER)
+if not os.path.isdir("py_common/" + TMP_FOLDER):
+    os.mkdir("py_common/" + TMP_FOLDER)
+
+#################
+### FUNCTIONS ###
+#################
+# return a proper built slug
+def build_slug(slug: str):
+    '''
+        a slug it is built in this way:
+            - removes all special characters, except for letters
+            - makes everything lowercase
+            - hyphens are used instead of spaces
+            - accented characters are normalized (ascii)
+    '''
+    # delete characters not needed in the slug
+    slug = re.sub("[^0-9a-zA-ZÀ-ÖØ-öø-ÿ]+", " ", slug)  # removes all except letters and numbers
+    slug = slug.lower() # to lowercase
+    slug = slug.strip().replace(" ", "-") # hypens instead of spaces
+    slug = unidecode(slug)  # normalize accented characters
+
+    return slug
+
+def find(pattern, path):
+    '''
+        find files matching a path in a folder and its subfolders
+    '''
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name, pattern):
+                result.append(os.path.join(root, name))
+
+    return result
+
+def gimme_global_games_list():
+    '''
+        return a list containing all slugs in entrypath 
+    '''
+    entries_list = listdir("../../entries")
+    
+    return(sorted(entries_list + listdir("py_common/" + BETA_FOLDER)) if DEBUG else sorted(entries_list))
+
+def fetch_prod_name(prod, suffix, filepath):
+    '''
+        return a list with path as the first entry if file is found in the unzippedfolder
+    '''
+    path = []           # manage the unknown extensions
+
+    # fetching product path in the unzippedfolder
+    if prod.platform == suffix.upper():     # e.g. if "GB" == "GB"
+        path = find("*." + suffix, filepath + "unzippedfolder")
+        
+    return path
+
+def build(prod: Production, entrypath: str):
+    '''
+        given a prod "Production" object containing
+        all production's data, create a proper named folder, fetches all files (screenshot + rom)
+        and properly organize everything 
+    '''
+    if not os.path.exists(entrypath + prod.slug):
+        #############
+        # PROD FILE #
+        #############
+        # make its own folder
+        os.mkdir(entrypath + prod.slug, 0o777)
+
+        # figuring out the suffix
+        suffix = str.lower(prod.url.split(".")[-1])
+
+        # building the filepath
+        filepath = entrypath + prod.slug + "/"
+
+        # download the file
+        # in case of http
+        if prod.url.startswith("http"):
+            r = requests.get(prod.url, allow_redirects=True, timeout=None, verify=False)
+            if r.status_code != 200:
+                logger.write("[ERR]:", str(r.status_code) + ": " + prod.slug + " - " + prod.url)
+
+                # cleaning in case of error
+                shutil.rmtree(entrypath + prod.slug)
+                return 1
+            
+            open(filepath + prod.slug + "." + suffix, 'wb').write(r.content)
+        else:
+            with contextlib.closing(urllib.request.urlopen(prod.url)) as r:
+                with open(filepath + prod.slug + "." + suffix, 'wb') as f:
+                    shutil.copyfileobj(r, f)
+        
+        # unzip in case of zip
+        if prod.url.endswith(".zip") or prod.url.endswith(".ZIP"):
+            # download and unzip
+            try:
+                with zipfile.ZipFile(filepath + prod.slug + "." + suffix,"r") as zip_ref:
+                    zip_ref.extractall(filepath + "unzippedfolder")
+
+                # manage all extensions, and it doesn't matter if they have uppercase or lowercase
+                path = []       # eventually the file
+                extensions = [ "gb", "GB", "gbc", "GBC"] # "gba", "GBA"]
+                for extension in extensions:
+                    path = fetch_prod_name(prod, extension, filepath)
+                    if path != []:
+                        break
+
+                # proper renaming and moving the file
+                if path != []:
+                    os.rename(path[0], filepath + prod.slug + "." + suffix)
+                else:
+                    logger.write("[WARN]",prod.title + " extension is not a " + prod.platform + " file.")
+                    shutil.rmtree(entrypath + prod.slug)
+                    return 1
+
+                # cleaning up unneeded files
+                shutil.rmtree(filepath + "unzippedfolder")
+                if CLEANZIP: os.remove(filepath + prod.slug + "." + "zip")
+            except zipfile.BadZipFile as e:
+                logger.write("[ERR] ", str(e) + " bad zip file")
+                shutil.rmtree(entrypath + prod.slug)
+                return 1
+        else:
+            # it is a proper gb file -> just write the filename in its own structure field
+            pass
+        
+        # update production object file
+        prod.files.append(prod.slug + "." + suffix)
+        
+        # download the screenshot
+        r = requests.get(prod.screenshots[0], allow_redirects=True, timeout=None)
+        open(filepath + prod.slug + "." + "png", 'wb').write(r.content)
+        prod.screenshots[0] = prod.slug + "." + "png"
+    else:
+        logger.write("[WARN]", "directory already present. Skipping " + prod.slug + "...")
+        return 1
+    return 0
+    
+def makeJSON(prod, entrypath):
+    '''
+        build the json file contained in each directory
+    '''
+    if os.path.exists(entrypath + prod.slug):
+        jsondata = {
+            "developer": prod.developer,
+            "files": [
+                {
+                    "default": True,
+                    "filename": prod.files[0],
+                    "playable": True
+                }
+            ],
+            "platform": prod.platform,
+            "repository": prod.repository,
+            "screenshots": [
+                prod.screenshots[0],
+            ],
+            "slug": prod.slug,
+            "title": prod.title,
+            "typetag": prod.typetag
+        }
+
+        updateJSON(jsondata, entrypath + prod.slug + "/game.json")
+    else:
+        logger.write("[ERR]", "Unable to create file for " + prod.slug + ". There is no directory for this prod.")
+        return 1
+    return 0
+
+def updateJSON(data, path):
+    jsonstr = json.dumps(data, sort_keys=True, indent=4)
+    jsonfile = open(path, "w")
+    jsonfile.write(jsonstr)
+    jsonfile.close()
