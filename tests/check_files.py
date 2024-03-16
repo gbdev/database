f1e7cf3ca50a1279e647c77b4bca1926105be27d
Antonio Vivace <avivace4@gmail.com>
2024-03-16 13:12:45 +0100

    check-files: add progressbar

--- tests/check_files.py
+++ tests/check_files.py
@@ -5,19 +5,26 @@ This script should be run after having verified that the game manifest validates
 
 import json
 import os
+import progressbar
+
 
 path = "../entries/"
 games_list = os.listdir(path)
 
-for game in games_list:
-
+for game in progressbar.progressbar(games_list, redirect_stdout=True):
     with open(f"../entries/{game}/game.json") as f:
         game = json.load(f)
     print(f"Checking {game['slug']}..")
     for file in game["files"]:
         if file["filename"] not in os.listdir(f"{path}/{game['slug']}"):
-            raise Exception(f'{file["filename"]} found in manifest but not on disk (entry {game["slug"]})')
+            raise Exception(
+                f'{file["filename"]} found in manifest but not on disk (entry {game["slug"]})'
+            )
     for screenshot in game["screenshots"]:
         if screenshot not in os.listdir(f"{path}/{game['slug']}"):
-            raise Exception(f'{screenshot} found in manifest but not on disk (entry {game["slug"]})')
-    print(f"{game['slug']}: {len(game['files'])} file(s), {len(game['screenshots'])} screenshot(s)")
+            raise Exception(
+                f'{screenshot} found in manifest but not on disk (entry {game["slug"]})'
+            )
+    print(
+        f"{game['slug']}: {len(game['files'])} file(s), {len(game['screenshots'])} screenshot(s)"
+    )
