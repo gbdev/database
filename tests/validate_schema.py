aa706f4443c9381ec15358f42c4369e48f8efc45
Antonio Vivace <avivace4@gmail.com>
2024-06-05 00:10:17 +0200

    tests: make validate schema return all failing validations

--- tests/validate_schema.py
+++ tests/validate_schema.py
@@ -5,8 +5,9 @@ Validate every game manifest against the JSON schema.
 import json
 import os
 import progressbar
+import sys
 
-from jsonschema import validate
+from jsonschema import validate, ValidationError
 
 with open("../game-schema-d4.json") as f:
     schema = json.load(f)
@@ -14,11 +15,19 @@ with open("../game-schema-d4.json") as f:
 path = "../entries/"
 games_list = os.listdir(path)
 n = 0
-
+errors = 0
 
 for game in progressbar.progressbar(games_list, redirect_stdout=True):
     n += 1
     print(f"Validating {game}..")
     with open(f"../entries/{game}/game.json") as f:
-        game = json.load(f)
-    validate(game, schema)
+        game_metadata = json.load(f)
+    try:
+        validate(game_metadata, schema)
+    except ValidationError as e:
+        errors += 1
+        print(f"Failed {game}: \n {e}")
+
+if errors > 0:
+    print(f"\n {errors} entrie(s) have failed validation")
+    sys.exit(1)
